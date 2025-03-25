0472699a6cb1c6bf9e9e51f961b00ab29d941734
nbenard <nbenard755@gmail.com>
2025-03-25 13:00:39 +0100

    add config setup and start for the epoll method

---
+++ src/config/config.rs
@@ -0,0 +1,61 @@
+use crate::epoll::path_server;
+
+use lazy_static::lazy_static;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+lazy_static! {
+    pub static ref PATH_SERVER : String = path_server();
+}
+
+// Example of config.txt
+// 127.0.0.1:7878
+// 127.0.0.1:7879 host.name
+// 127.0.0.1 : 7878
+
+pub fn config_output(path : &str) -> Vec<(String ,u16 ,String)> {
+    let mut output = Vec::new();
+
+    let file = match File::open(path) {
+        Ok(file) => file,
+        Err(_) => {
+            eprintln!("Error opening config file at {}", path);
+            return output; //empty output
+        }
+    };
+
+    let reader = BufReader::new(file);
+    
+    for line in reader.lines() {
+        if let Ok(line) = line {
+            let parts = parse_str(&line, ' ');
+
+            if parts.len() == 1 {
+                let part = parse_str(parts[0], ':');
+                let (ip, port) = (part[0], part[1]);
+
+                if let Ok(port) = port.parse::<u16>() {
+                    output.push((ip.to_string(), port,"".to_string()));
+                } else {
+                    eprintln!("Error parsing port number");
+                }
+            } else if parts.len() == 2 {
+                let part = parse_str(parts[0], ':');
+                let (ip, port) = (part[0], part[1]);
+                let domain_name = parts[1];
+                
+                if let Ok(port) = port.parse::<u16>() {
+                    output.push((ip.to_string(), port, domain_name.to_string()));
+                    println!("{}", port);
+                } else {
+                    eprintln!("Error parsing port number {}", port);
+                }
+            }
+        }
+    }
+    return output;
+}
+ 
+fn parse_str(input: &str, sep : char) -> Vec<&str> {
+    input.split(sep).collect()
+}
---
+++ src/connect/epoll.rs
@@ -0,0 +1,61 @@
+use libc::{epoll_create1, epoll_ctl, epoll_wait, EPOLLIN, EPOLL_CTL_ADD};
+use std::os::unix::io::RawFd;
+use std::process::Command;
+
+#[derive(Debug)]
+pub struct Epoll {
+    epoll_fd: RawFd,
+}
+
+impl Epoll {
+    pub fn new() -> Self {
+        let epoll_fd = unsafe { epoll_create1(0) };
+        assert!(epoll_fd >= 0);
+        Self { epoll_fd }
+    }
+
+    pub fn add_fd(&self, fd: RawFd) {
+        let mut event = libc::epoll_event {
+            events: EPOLLIN as u32,
+            u64: fd as u64,
+        };
+        unsafe {
+            epoll_ctl(self.epoll_fd, EPOLL_CTL_ADD, fd, &mut event);
+        }
+    }
+
+    pub fn wait(&self, max_events: usize) -> Vec<RawFd> {
+        let mut events = vec![libc::epoll_event { events: 0, u64: 0 }; max_events];
+        let num_events = unsafe { epoll_wait(self.epoll_fd, events.as_mut_ptr(), max_events as i32, -1) };
+        assert!(num_events >= 0);
+
+        events.into_iter().take(num_events as usize).map(|e| e.u64 as RawFd).collect()
+    }
+}
+
+pub fn path_server() -> String {
+    if cfg!(target_os = "linux") {
+        match Command::new("pwd").output() {
+            Ok(output) => {
+                if output.status.success() {
+                    match String::from_utf8(output.stdout) {
+                        Ok(stdout) => stdout.trim().to_string(),
+                        Err(_) => {
+                            eprintln!("Error converting output");
+                            String::new()
+                        }
+                    }
+                } else {
+                    eprintln!("Error when searching for path");
+                    String::new()
+                }},
+            Err(_) => {
+                eprintln!("Error when executing command for path");
+                String::new()
+            }
+        }
+    } else {
+        eprintln!("This OS is not supported");
+        String::new()
+    }
+}
