a97f51b9f58e96d6c1f18a051fcb5a6a2efd96be
nbenard <nbenard755@gmail.com>
2025-09-11 16:22:49 +0200

    prepare for audit

--- src/http/router.rs
+++ src/http/router.rs
@@ -1,6 +1,7 @@
 use crate::cgi_bin::handle;
 use crate::errors::handler::error_response;
 use crate::http::methods::*;
+use crate::http::response::create_response;
 use std::net::TcpStream;
 
 use super::request::HttpRequest;
@@ -15,6 +16,10 @@ pub fn route_request(req: HttpRequest, stream: &mut TcpStream) {
     }
 
     match req.method.as_str() {
+        "GET" if req.path == "/empty" => {
+            let response = create_response("200 OK", "", "text/plain", None);
+            response.send(stream, &req);
+        }
         "GET" => get::handle_get(&req, stream),
         "POST" => post::handle_post(&req, stream),
         "DELETE" => delete::handle_delete(&req, stream),
